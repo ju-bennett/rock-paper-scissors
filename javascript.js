3333b8f4a10bef4799b2b0fc49ac96cd403bb55e
ju-bennett <jules.d.bennett@gmail.com>
2025-02-12 09:11:15 -0800

    Finish rock paper scissors game

--- javascript.js
+++ javascript.js
@@ -1,22 +1,77 @@
-console.log ("Hello World");
-
-
-// Create a new function named getComputerChoice
-    // Create a variable named randomNumber and set it to a random number between 0 and 1
-        // If the number is less than 1/3, return "rock"
-        // If the number is greater than or equal to 1/3 but less than 2/3, return "paper"
-        // If the number is greater than or equal to 2/3, return "scissors"
-
-function getComputerChoice (){ 
+// Function to get computer's choice
+function getComputerChoice() {
     let randomNumber = Math.random();
-    if (randomNumber < 1/3){
+    if (randomNumber < 1/3) {
         return "rock";
-    }
-    if (randomNumber < 2/3){
+    } else if (randomNumber < 2/3) {
         return "paper";
-    }
-    else {
+    } else {
         return "scissors";
     }
 }
-console.log(getComputerChoice());
+
+// Function to get human's choice
+function getHumanChoice() {
+    let humanChoice = prompt("Do you choose rock, paper, or scissors?").toLowerCase();
+    while (!["rock", "paper", "scissors"].includes(humanChoice)) {
+        humanChoice = prompt("Invalid choice! Please choose rock, paper, or scissors:").toLowerCase();
+    }
+    return humanChoice;
+}
+
+// Function to play a single round
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie! You both chose ${humanChoice}.`);
+        return "tie";
+    }
+
+    if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+        console.log(`You win this round! ${humanChoice} beats ${computerChoice}.`);
+        return "human";
+    } else {
+        console.log(`The computer wins this round! ${computerChoice} beats ${humanChoice}.`);
+        return "computer";
+    }
+}
+
+// Function to play the game
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let round = 1; round <= 5; round++) {
+        console.log(`\nRound ${round}`);
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        let roundResult = playRound(humanSelection, computerSelection);
+
+        if (roundResult === "human") {
+            humanScore++;
+        } else if (roundResult === "computer") {
+            computerScore++;
+        }
+
+        console.log(`Current Score - You: ${humanScore}, Computer: ${computerScore}`);
+    }
+
+    // Determine the final winner
+    console.log("\nFinal Results:");
+    if (humanScore > computerScore) {
+        console.log("You win the game! 🎉");
+    } else if (computerScore > humanScore) {
+        console.log("The computer wins the game! 🤖");
+    } else {
+        console.log("It's a tie game! 😐");
+    }
+}
+
+// Start the game
+playGame();
+
+
